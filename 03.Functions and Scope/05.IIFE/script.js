8e242a19e337d629f509b3b34852cd0cee7e266c
DEBASISH SAHOO <debasish.sahoo96@gmail.com>
2025-09-15 15:39:19 +0530

    update4

---
+++ 03.Functions and Scope/05.IIFE/script.js
@@ -0,0 +1,49 @@
+//Immediately Invoked Function Expressions
+//()() - function is defined and immediately invoked
+
+// Basic IIFE
+(function () {
+  console.log("IIFE executed immediately!");
+})();
+
+// IIFE with parameters
+(function (name, version) {
+  console.log(`Welcome to ${name} v${version}`);
+})("MyApp", "1.0.0");
+
+// IIFE returning a value
+const result = (function (x, y) {
+  return x * y + 10;
+})(5, 3);
+console.log(result); // 25
+
+// IIFE for module pattern
+const MyModule = (function () {
+  let privateVariable = 0;
+  let privateArray = [];
+  function privateFunction() {
+    console.log("This is private");
+  }
+  return {
+    publicMethod: function () {
+      privateVariable++;
+      privateFunction();
+      console.log("Public method called", privateArray);
+      return privateVariable;
+    },
+    addItem: function (item) {
+      privateArray.push(item);
+    },
+    getItems: function () {
+      return [...privateArray]; // Return copy, not reference
+    },
+    reset: function () {
+      privateVariable = 0;
+      privateArray = [];
+    },
+  };
+})();
+console.log(MyModule.publicMethod());
+MyModule.addItem("apple");
+MyModule.addItem("banana");
+console.log(MyModule.getItems());
